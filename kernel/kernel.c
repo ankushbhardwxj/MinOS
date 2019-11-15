f91206acc702a2bcbae57befa7e8724622913b6d
ankingcodes <ankush.bhardwaj0@gmail.com>
2019-11-15 17:58:23 +0530

    modified kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,10 @@
 #include "../drivers/screen.c"
-
 void main() {
     clear_screen();
-    print_at("This text spans multiple lines", 75,10);
-    print_at("This is a line \n break",0,20);
-    print_at("Ankush Bhardwaj",23,12);
+    print_at("X", 1, 6);
+    print_at("This text spans multiple lines", 75, 10);
+    print_at("There is a line\nbreak", 0, 20);
+    print("There is a line\nbreak");
+    print_at("What happens when we run out of space?", 45, 24);
 }
+
